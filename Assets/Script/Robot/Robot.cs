800e91b9aa080f829d351439b128b02e831b7b82
NourTantawy <nour.amr.tantawy@gmail.com>
2025-09-30 20:27:15 +0300

    Robot Agent and follow player

---
+++ Assets/Script/Robot/Robot.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Robot : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private NavMeshAgent agent;
+    
+    [SerializeField] private float rotationSpeed = 720f; 
+    [SerializeField] private float yawOffset = 180f;       // set 180 if your model faces backward
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        FollowPlayer();
+    }
+
+    private void FollowPlayer()
+    {
+        agent.updateRotation = false;
+        agent.updatePosition = true;
+        agent.SetDestination(player.transform.position);
+        
+        Vector3 toPlayer = player.transform.position - transform.position;
+        toPlayer.y = 0f; 
+        if (toPlayer.sqrMagnitude > 0.0001f)
+        {
+            Quaternion target = Quaternion.LookRotation(toPlayer) * Quaternion.Euler(0f, yawOffset, 0f);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, target, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
