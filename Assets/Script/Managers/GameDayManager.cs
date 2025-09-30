88744fce2e9bf8f1b1a9df92b185701e368cbaa1
ahmedtarek52 <117595019+ahmedtarek52@users.noreply.github.com>
2025-09-30 20:38:52 +0300

    create game manager

---
+++ Assets/Script/Managers/GameDayManager.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameTask
+{
+    PlayPC,
+    ReadBook,
+    PlayPiano,
+    DrinkCoffee,
+    // Add more tasks as needed
+}
+
+public class GameDayManager : MonoBehaviour
+{
+    public static GameDayManager instance;
+
+    [SerializeField] int currentDay = 1;
+    public int CurrentDay => currentDay;
+
+    // Map each day to its allowed tasks
+    private Dictionary<int, HashSet<GameTask>> dayTasks = new Dictionary<int, HashSet<GameTask>>()
+    {
+        { 1, new HashSet<GameTask> { GameTask.PlayPC, GameTask.ReadBook } },
+        { 2, new HashSet<GameTask> { GameTask.PlayPiano, GameTask.DrinkCoffee } },
+        { 3, new HashSet<GameTask> { /* Add tasks for day 3 */ } },
+        { 4, new HashSet<GameTask> { /* Add tasks for day 4 */ } },
+        { 5, new HashSet<GameTask> { /* Add tasks for day 5 */ } },
+    };
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(gameObject);
+    }
+
+    public bool IsTaskAllowed(GameTask task)
+    {
+        if (dayTasks.TryGetValue(currentDay, out var allowedTasks))
+            return allowedTasks.Contains(task);
+        return false;
+    }
+
+    public void AdvanceDay()
+    {
+        currentDay = Mathf.Clamp(currentDay + 1, 1, dayTasks.Count);
+        // Optionally trigger events/UI updates here
+    }
+
+    public void SetDay(int day)
+    {
+        currentDay = Mathf.Clamp(day, 1, dayTasks.Count);
+    }
+}
